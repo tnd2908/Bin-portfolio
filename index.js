e476169cd3324c1307b2e08db33ca01b6ceb5cb3
tnd2908 <tnd.290800@gmail.com>
2022-01-14 00:28:12 +0700

    aaa

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 const pageRouter = require('./Route/page')
 app.set('view engine', 'ejs')
 // app.use('/', pageRouter)
-app.get('/', (req, res)=> res.render('home'))
+app.get('/', (req, res)=> res.render('home.ejs'))
 const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`))
