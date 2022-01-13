c3a6891a7283126a38f3ea21c86faa4639167580
tnd2908 <tnd.290800@gmail.com>
2022-01-13 21:37:51 +0700

    fix port

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ app.use(express.static('public'))
 const pageRouter = require('./Route/page')
 app.set('view engine', 'ejs')
 app.use('/', pageRouter)
-app.listen(4000, () => console.log('Server is running on 4000'))
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log('Server is running on 4000'))
