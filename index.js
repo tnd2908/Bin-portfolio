9d9a842c685b6c96c5b0ec900168367b8861b379
tnd2908 <tnd.290800@gmail.com>
2022-01-14 00:26:14 +0700

    acbask

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'))
 const pageRouter = require('./Route/page')
 app.set('view engine', 'ejs')
-app.use('/', pageRouter)
+// app.use('/', pageRouter)
+app.get('/', (req, res)=> res.render('home'))
 const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`))
