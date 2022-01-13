29cfd094cc886f7221a27550dd1d2e4b144907fa
tnd2908 <tnd.290800@gmail.com>
2022-01-13 21:52:12 +0700

    a

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const http = require('http')
 const ejs = require('ejs') 
 app.use(express.static('public'))
 const pageRouter = require('./Route/page')
 app.set('view engine', 'ejs')
 app.use('/', pageRouter)
 const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.log('Server is running on 4000'))
+const server = http.createServer(app)
+server.listen(PORT, () => console.log('Server is running on 4000'))
