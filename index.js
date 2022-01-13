b1b0903f5e071afb54ec3ec85307937776a27da3
tnd2908 <tnd.290800@gmail.com>
2022-01-14 00:47:27 +0700

    final

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const http = require('http')
-// const ejs = require('ejs')
+const ejs = require('ejs')
 app.use(express.static('public'))
-// const pageRouter = require('./Route/page')
-// app.set('views', './views');
-// app.set('view engine', 'ejs')
-// app.use('/', pageRouter)
+const pageRouter = require('./Route/page')
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs')
+app.use('/', pageRouter)
 app.get('/', (req, res)=> res.send('<h1>Hello</h1>'))
 const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
-server.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running on ${PORT} `))
