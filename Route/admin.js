72d860797fc542c39d9fa29fc578e09086ad0dcd
dan <dan.tran@orientsoftware.com>
2022-07-12 18:24:47 +0700

    asd

--- Route/admin.js
+++ Route/admin.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const AuthController = require('../Controller/AuthController');
+const Project = require('../Model/Project');
 const adminRouter = express.Router();
 adminRouter.get('/login', (req, res) => {
     res.render('login', {msg: ''})
 })
-adminRouter.get('/', (req, res) => {
-    res.render('admin')
+adminRouter.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find({}) || []
+        res.render('admin', {projects})
+    } catch (error) {
+        
+    }
 })
 adminRouter.get('/add', (req, res) => {
     res.render('add-project')
